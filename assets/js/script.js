189a2cf2011933299df5f4a20969df4318decdc7
Elias Ortega <eliasortega2011@gmail.com>
2024-04-01 20:29:56 -0300

    Se agrega footer. Scrip js tooltip y alert en boton, toggle cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 $(document).scroll(function (e) {
+    //cambio color  de barra de navegacion
     const y = $("html").scrollTop();
     if (y > 300) $("nav").addClass("nav-black")
     else $("nav").removeClass("nav-black")
 })
+
+$(document).ready(function () {
+    //tooltip del boton enviar
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $('#btnEnviar').click(function (event) {
+        event.preventDefault()
+        var nombre = $('#txtNombre').val();
+        var asunto = $('#txtAsunto').val();
+        var mensaje = $('#txtArea').val();
+
+        if (nombre !== "" && asunto !== "" && mensaje !== "") {
+
+            alert('El mensaje fue enviado correctamente...');
+
+            $('#txtNombre').val('');
+            $('#txtAsunto').val('');
+            $('#txtArea').val('');
+        }
+    });
+
+    //Oculta y muestra el parrafo de las tarjetas.
+    $(".card-title").click(function () {
+        $(this).next('.card-text').toggle();
+    });
+
+
+});
